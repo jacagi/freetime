76e6604a38747f61bf3fd0d02c0617f52eb65d3c
jacagi <jacagi@inf.upv.es>
2021-12-08 17:49:34 +0100

    More changes day6 advent of code

--- AdventofCode_2021/day6.py
+++ AdventofCode_2021/day6.py
@@ -1,23 +1,17 @@
 from collections import Counter
 
-def myFunc(fish, days):
+def fish_generator(fish, days):
     fish_count = Counter(fish)
-    fish_per_day = []
-    for i in range(9):
-        if fish_count[i]:
-            fish_per_day.append(fish_count[i])
-        else:
-            fish_per_day.append(0)
+    fish_per_day = [fish_count[i] if fish_count[i] else 0 for i in range(9)]
     for _ in range(days):
         new_fish = fish_per_day[0]
         for key in range(9):
-            if key != 8: fish_per_day[key] = fish_per_day[key + 1] 
-        fish_per_day[8]=new_fish
+            fish_per_day[key] = fish_per_day[key + 1] if key != 8 else new_fish
         fish_per_day[6]+=new_fish
     return sum(fish_per_day)
 
 print("-*-*-*-*-*-*-*-*-*-*-*-")
 myList=list(map(int,open("input6.txt","r").read().replace("\n","").split(",")))
-print("Day 6 Part 1: " + str(myFunc(myList, 80)))
-print("Day 6 Part 2: " + str(myFunc(myList, 256)))
+print("Day 6 Part 1: " + str(fish_generator(myList, 80)))
+print("Day 6 Part 2: " + str(fish_generator(myList, 256)))
 print("-*-*-*-*-*-*-*-*-*-*-*-")
